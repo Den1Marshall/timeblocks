de206a95f5a2ef685b4ec55d83b6be34a804771a
Den Marshall <dank03052005@gmail.com>
2023-07-01 14:39:48 +0300

    Added: roboto font, RTK store and provider, temporarily removed strict mode in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
+  // </React.StrictMode>
 );
