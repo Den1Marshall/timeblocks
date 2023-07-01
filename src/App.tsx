6b38c9d2d33b7e6fff68cd9207305155f29c3591
Den Marshall <dank03052005@gmail.com>
2023-07-01 14:54:12 +0300

    Added: implemented wakeLock func, persist gate, routing, WakeLockModal, body bg depending on color mode change in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,50 @@
 import { FC } from 'react';
-import Home from './pages/Home';
+import MyBlocks from './pages/MyBlocks';
 import { ThemeProvider } from '@emotion/react';
 import makeTheme from './utils/makeTheme';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './layouts/Layout';
+import { useSelector } from 'react-redux';
+import { RootState, persistor } from './redux/store';
+import Settings from './pages/Settings';
+import Home from './pages/Home';
+import { PersistGate } from 'redux-persist/integration/react';
+import WakeLockModal from './components/WakeLockModal';
+import doWakeLock, { releaseWakeLock } from './utils/wakeLock';
 
 const App: FC = () => {
-  const theme = makeTheme('dark');
+  const colorMode = useSelector(
+    (state: RootState) => state.colorModeReducer.colorMode
+  );
+  const wakeLockEnabled = useSelector(
+    (state: RootState) => state.wakeLockReducer.enabled
+  );
+
+  document.body.style.backgroundColor = colorMode === 'dark' ? '#000' : '#fff';
+
+  const theme = makeTheme(colorMode);
+
+  if (wakeLockEnabled) {
+    doWakeLock();
+  } else {
+    releaseWakeLock();
+  }
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route index element={<Home />}></Route>
-            <Route path='*' element={<Home />}></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <PersistGate persistor={persistor}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path='/my-blocks' element={<MyBlocks />} />
+              <Route path='/settings' element={<Settings />} />
+              <Route path='*' element={<MyBlocks />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+        <WakeLockModal />
+      </ThemeProvider>
+    </PersistGate>
   );
 };
 
