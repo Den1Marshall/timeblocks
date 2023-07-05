2532febc04604eebcb85160dd54d2466337340bd
Den Marshall <dank03052005@gmail.com>
2023-07-05 14:20:06 +0300

    Removed: old comments in TimeBlock.ts

--- src/utils/TimeBlock.ts
+++ src/utils/TimeBlock.ts
@@ -13,13 +13,4 @@ export interface TimeBlock {
   // public doneForWeek = 0;
   // public doneForMonth = 0;
   // public doneForYear = 0;
-
-  // public restBetweenSubBlocks?: number;
-  // public subBlocks?: number;
-
-  // constructor(id: number, name: string, time: number, color = 'primary.main') {
-  //   this.name = name;
-  //   this.time = time;
-  //   this.id = id;
-  //   this.color = color;
 }
