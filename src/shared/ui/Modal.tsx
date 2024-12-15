c889d2effaf74e64a714249c4b2abc2f5df48319
Denys Hrychulevych <111864961+Den1Marshall@users.noreply.github.com>
2024-12-16 00:23:31 +0200

    fix(Modal): use custom motionProps to prevent nextui modal bug

--- src/shared/ui/Modal.tsx
+++ src/shared/ui/Modal.tsx
@@ -14,20 +14,39 @@ export const Modal: FC<NextUIModalProps> = ({ children, ...rest }) => {
   const modalRef = useRef<HTMLDivElement>(null);
   const modalHeight = modalRef.current?.getBoundingClientRect().height;
 
-  const motionProps: MotionProps = {
+  const mobileMotionProps: MotionProps = {
     initial: {
       transform: 'translateY(50%)',
     },
+
     animate: { transform: 'translateY(0%)' },
+
     exit: {
       transform: `translateY(${modalHeight + 'px'})`,
+    },
+  };
+
+  const motionProps: MotionProps = {
+    initial: {
+      transform: 'scale(1.03)',
+      opacity: 0,
+    },
+
+    animate: {
+      transform: 'scale(1)',
+      opacity: 1,
+    },
+
+    exit: {
+      transform: 'scale(1.03)',
+      opacity: 0,
     },
   };
 
   return (
     <NextUIModal
       ref={modalRef}
-      motionProps={isMobile ? motionProps : undefined}
+      motionProps={isMobile ? mobileMotionProps : motionProps}
       classNames={{
         base: 'max-sm:h-[auto] max-sm:max-h-[50%] max-sm:overflow-y-auto max-sm:min-w-full max-sm:pb-safe max-sm:mb-0 max-sm:mx-0',
         closeButton: 'max-sm:hidden',
