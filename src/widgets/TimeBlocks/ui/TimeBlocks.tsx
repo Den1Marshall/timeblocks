7ebed4a2f36b3f570b30db33b99c66fdab8f9a73
Denys Hrychulevych <111864961+Den1Marshall@users.noreply.github.com>
2025-01-19 19:58:31 +0200

    WIP: fix

--- src/widgets/TimeBlocks/ui/TimeBlocks.tsx
+++ src/widgets/TimeBlocks/ui/TimeBlocks.tsx
@@ -4,14 +4,11 @@ import {
   ITimeBlock,
   TimeBlock,
 } from '@/entities/TimeBlock';
-import { FC, ReactNode, useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '@/app/redux';
+import { FC, ReactNode } from 'react';
+import { useAppSelector } from '@/app/redux';
 import { ControlTimeBlock } from '@/features/ControlTimeBlock';
 import { DeleteTimeBlock } from '@/features/DeleteTimeBlock';
 import { AnimatePresence } from 'motion/react';
-import { doc, onSnapshot } from 'firebase/firestore';
-import { db } from '@/shared/config';
-import { timeBlocksSliceActions } from '../model/timeBlocksSlice';
 
 interface TimeBlocksProps {
   AddTimeBlock: ReactNode;
@@ -25,35 +22,6 @@ export const TimeBlocks: FC<TimeBlocksProps> = ({
   const timeBlocks = deserializeTimeBlocks(
     useAppSelector((state) => state.timeBlocksSliceReducer.timeBlocks)
   );
-  const userUid = useAppSelector((state) => state.userSliceReducer.user?.uid);
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    if (!userUid) return;
-
-    const unsub = onSnapshot(doc(db, 'users', userUid), (doc) => {
-      const serverTimeBlocks = JSON.parse(
-        doc.get('timeBlocks') ?? JSON.stringify([])
-      );
-
-      if (
-        JSON.stringify(
-          serverTimeBlocks.map((serverTimeBlock: ITimeBlock) => ({
-            ...serverTimeBlock,
-            elapsed: null,
-          }))
-        ) ===
-        JSON.stringify(
-          timeBlocks.map((timeBlock) => ({ ...timeBlock, elapsed: null }))
-        )
-      )
-        return;
-
-      dispatch(timeBlocksSliceActions.initializeTimeBlocks(serverTimeBlocks));
-    });
-
-    return () => unsub();
-  }, [userUid, dispatch]);
 
   return (
     <section className='mt-10 w-full h-full flex flex-wrap justify-center items-start gap-10 overflow-y-auto no-scrollbar lg:justify-start'>
