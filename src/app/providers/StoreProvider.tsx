dd4c41da6dbf6fdf6a968e79e65afd6f0abd0f1c
Denys Hrychulevych <111864961+Den1Marshall@users.noreply.github.com>
2025-01-20 19:18:10 +0200

    fix(StoreProvider): WIP move real-time synchronization of TimeBlocks from TimeBlocks widget to StoreProvider to prevent extra requests

--- src/app/providers/StoreProvider.tsx
+++ src/app/providers/StoreProvider.tsx
@@ -1,10 +1,12 @@
 'use client';
-import { FC, PropsWithChildren, useRef } from 'react';
+import { FC, PropsWithChildren, useEffect, useRef } from 'react';
 import { Provider } from 'react-redux';
 import { makeStore, type AppStore } from '../redux';
 import { IUser, userSliceActions } from '@/entities/User';
 import { timeBlocksSliceActions } from '@/widgets/TimeBlocks';
 import { ITimeBlock } from '@/entities/TimeBlock';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { db } from '@/shared/config';
 
 interface StoreProviderProps extends PropsWithChildren {
   user?: IUser;
@@ -25,6 +27,22 @@ export const StoreProvider: FC<StoreProviderProps> = ({
       timeBlocksSliceActions.initializeTimeBlocks(timeBlocks)
     );
   }
+
+  useEffect(() => {
+    if (!user?.uid || !storeRef?.current) return; // ???
+
+    const unsub = onSnapshot(doc(db, 'users', user.uid), (doc) => {
+      const serverTimeBlocks = JSON.parse(
+        doc.get('timeBlocks') ?? JSON.stringify([])
+      );
+
+      storeRef?.current?.dispatch(
+        timeBlocksSliceActions.initializeTimeBlocks(serverTimeBlocks)
+      );
+    });
+
+    return () => unsub();
+  }, [user?.uid]);
 
   return <Provider store={storeRef.current}>{children}</Provider>;
 };
