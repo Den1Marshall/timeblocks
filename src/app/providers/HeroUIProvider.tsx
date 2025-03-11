b1638ef31a4093e31b987039980fcad6af464dd2
Denys Hrychulevych <111864961+Den1Marshall@users.noreply.github.com>
2025-03-11 23:56:55 +0200

    feat(HeroUIProvider): use defaultTransition in toast motion properties

--- src/app/providers/HeroUIProvider.tsx
+++ src/app/providers/HeroUIProvider.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/navigation';
 import { ThemeProvider } from 'next-themes';
 import { useLocale } from 'next-intl';
 import { useMediaQuery } from 'usehooks-ts';
+import { defaultTransition } from '@/shared/ui';
 
 export const HeroUIProvider: FC<PropsWithChildren> = ({ children }) => {
   const router = useRouter();
@@ -24,7 +25,10 @@ export const HeroUIProvider: FC<PropsWithChildren> = ({ children }) => {
       <ThemeProvider attribute='class'>
         <ToastProvider
           placement={md ? undefined : 'top-center'}
-          toastProps={{ classNames: { base: 'max-sm:mt-safe-or-4' } }}
+          toastProps={{
+            classNames: { base: 'max-sm:mt-safe-or-4' },
+            motionProps: { transition: defaultTransition },
+          }}
         />
 
         {children}
